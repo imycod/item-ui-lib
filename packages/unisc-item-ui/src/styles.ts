c6da535d99d5d49e255c6559d0e335ebaa6943fe
Jay Wu <jay.wu@item.com>
2024-07-11 11:30:47 +0800

    feat:按需加载样式

--- packages/unisc-item-ui/src/styles.ts
+++ packages/unisc-item-ui/src/styles.ts
@@ -1,6 +1,10 @@
 import "./style/style.css";
 
 import "./button/style/index.less";
+
+import "element-plus/es/components/dialog/style/index";
+import "element-plus/es/components/progress/style/index";
+
 import "./print-progress/style/index.less";
 
 // import "element-plus/theme-chalk/index.css";
