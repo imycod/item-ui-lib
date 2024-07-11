430f5b3462894f6ef26778587f64b919e5996a79
Jay Wu <jay.wu@item.com>
2024-07-11 09:39:11 +0800

    feat:tailwindcss

--- packages/unisc-item-ui/src/styles.ts
+++ packages/unisc-item-ui/src/styles.ts
@@ -6,4 +6,5 @@ import "./print-progress/style/index.less";
 // import "element-plus/theme-chalk/index.css";
 import "element-plus/theme-chalk/src/base.scss";
 import "element-plus/theme-chalk/src/progress.scss";
+import "element-plus/theme-chalk/src/popover.scss";
 import "element-plus/theme-chalk/src/dialog.scss";
