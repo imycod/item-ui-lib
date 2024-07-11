fd03776822edb73b123afeb085b4416bffd50d67
Jay Wu <jay.wu@item.com>
2024-07-11 11:24:43 +0800

    feat:按需加载样式

--- packages/unisc-item-ui/vite.config.ts
+++ packages/unisc-item-ui/vite.config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+// @ts-nocheck
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -6,16 +8,39 @@ import { tsxAutoProps } from "vite-plugin-tsx-auto-props";
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import ElementPlus from 'unplugin-element-plus/vite'
+import ElementPlus from 'unplugin-element-plus/vite';
+import {createStyleImportPlugin,ElementPlusResolve} from 'vite-plugin-style-import';
 
 // 读取我们当前的根目录
 export default defineConfig({
   // 首先就是我们的插件，插件我们直接找打我们跟目录下的vite.config.ts的文件
   plugins: [
-    ElementPlus({
-      // 导入scss而不是css
-      useSource: true
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
     }),
+    Components({
+      resolvers: [
+        ElementPlusResolver()
+      ],
+    }),
+    ElementPlus({
+      useSource: false
+    }),
+    // createStyleImportPlugin({
+    //   resolves: [
+    //     ElementPlusResolve(),
+    //   ],
+    //   libs: [
+    //     {
+    //       libraryName: 'element-plus',
+    //       // esModule: true,
+    //       resolveStyle: (name) => {
+    //         console.log('name---',name)
+    //         return `element-plus/lib/theme-chalk/${name}.css`;
+    //       },
+    //     },
+    //   ],
+    // }),
     dts({
       entryRoot: "src",
       outDir: ["lib", "es"],
