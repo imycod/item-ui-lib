4ec3e06c317b8f30e00ac0a09273da5c8dc21137
Jay Wu <jay.wu@item.com>
2024-08-02 19:00:47 +0800

    feat:opt menu icon

--- packages/item-ui/src/sidebar/SidebarItem.vue
+++ packages/item-ui/src/sidebar/SidebarItem.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import {
-  type PropType,
-  type CSSProperties,
-  ref,
-  toRaw,
-  computed,
-  useAttrs
-} from "vue";
+import { useAttrs } from "vue";
 import {
   Minus,
   Plus,
@@ -25,12 +18,16 @@ const props = defineProps({
 
 <template>
   <el-menu-item v-if="!item.children" :index="item.index">
-    <img :src="item.icon">
+    <el-icon>
+      <img :src="item.icon">
+    </el-icon>
     <span>{{ item.title }}</span>
   </el-menu-item>
   <el-sub-menu :expand-close-icon="Plus" :expand-open-icon="Minus" v-else teleported :index="item.index" v-bind="attrs">
     <template #title>
-      <img :src="item.icon">
+      <el-icon>
+        <img :src="item.icon">
+      </el-icon>
       <div> {{ item.title }}</div>
     </template>
     <sidebar-item v-for="child in item.children" :key="child.path" :item="child" />
